cab1ab0e635f2c4dac565477878304dfea982c0a
uamanager <uamanager@uamac3.local>
2024-10-26 10:35:05 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/Yd0q9xRRSV

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'investment-income-tax',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/investment-income-tax',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
